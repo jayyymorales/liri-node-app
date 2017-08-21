55cfdce731c50ff5af6d5a0fafcd75c926fe38d5
Jose Morales <josemorales@Joses-MacBook-Pro.local>
2017-08-21 14:49:40 -0700

    init

---
+++ keys.js
@@ -0,0 +1,9 @@
+// JavaScript
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '898016280917139457-Wu0mBm4V83pQ9hYJS5LjYTLOmIyrJCu',
+  consumer_secret: '898016280917139457-Wu0mBm4V83pQ9hYJS5LjYTLOmIyrJCu',
+  access_token_key: '898016280917139457-Wu0mBm4V83pQ9hYJS5LjYTLOmIyrJCu',
+  access_token_secret: '898016280917139457-Wu0mBm4V83pQ9hYJS5LjYTLOmIyrJCu',
+}
